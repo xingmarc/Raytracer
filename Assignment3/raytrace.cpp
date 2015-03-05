467e161e0e7170d22a467405c766826def94cbcf
Xing <xingmarc@yeah.net>
2015-03-05 09:28:32 -0800

    added the parsing function and the testForDebug

--- Assignment3/raytrace.cpp
+++ Assignment3/raytrace.cpp
@@ -22,6 +22,26 @@ struct Ray
 
 // TODO: add structs for spheres, lights and anything else you may need.
 
+//SPHERE <name> <pos x> <pos y> <pos z> <scl x> <scl y> <scl z> <r> <g> <b> <Ka> <Kd> <Ks> <Kr> <n>
+//LIGHT <name> <pos x> <pos y> <pos z> <Ir> <Ig> <Ib>
+struct sphere
+{
+    string name;
+    float pos_x,pos_y,pos_z;
+    float scl_x,scl_y,scl_z;
+    float r,g,b;
+    float Ka,Kd,Ks,Kr;
+    float n;
+};
+vector<sphere> spheres;
+struct light
+{
+    string name;
+    float pos_x,pos_y,pos_z;
+    float Ir,Ig,Ib;
+};
+vector<light> lights;
+
 vector<vec4> g_colors;
 
 float g_left;
@@ -29,6 +49,17 @@ float g_right;
 float g_top;
 float g_bottom;
 float g_near;
+
+struct backGroundColor{
+    float r,g,b;
+}back;
+struct ambientIntensity{
+    float Ir,Ig,Ib;
+}ambient;
+
+//output file name:
+string outputFileName;
+
 
 
 // -------------------------------------------------------------------
@@ -54,11 +85,65 @@ float toFloat(const string& s)
 void parseLine(const vector<string>& vs)
 {
     //TODO: add parsing of NEAR, LEFT, RIGHT, BOTTOM, TOP, SPHERE, LIGHT, BACK, AMBIENT, OUTPUT.
+    
+    if (vs[0] == "NEAR") {g_near = toFloat(vs[1]);}
+    else if(vs[0] == "LEFT"){g_left = toFloat(vs[1]);}
+    else if(vs[0] == "RIGHT"){g_right = toFloat(vs[1]);}
+    else if(vs[0] == "TOP"){g_top = toFloat(vs[1]);}
+    else if(vs[0] == "BOTTOM"){g_bottom = toFloat(vs[1]);}
+    
+    //RES (Resolution)
+    else
     if (vs[0] == "RES")
     {
         g_width = (int)toFloat(vs[1]);
         g_height = (int)toFloat(vs[2]);
         g_colors.resize(g_width * g_height);
+    }
+    else if(vs[0] == "BACK"){
+        back.r = toFloat(vs[1]);
+        back.g = toFloat(vs[2]);
+        back.b = toFloat(vs[3]);
+    }
+    else if(vs[0] == "AMBIENT"){
+        ambient.Ir = toFloat(vs[1]);
+        ambient.Ig = toFloat(vs[2]);
+        ambient.Ib = toFloat(vs[3]);
+    }
+    else if(vs[0] == "SPHERE"){
+        sphere s;
+        s.name = vs[1];
+        s.pos_x = toFloat(vs[2]);
+        s.pos_y = toFloat(vs[3]);
+        s.pos_z = toFloat(vs[4]);
+        s.scl_x = toFloat(vs[5]);
+        s.scl_y = toFloat(vs[6]);
+        s.scl_z = toFloat(vs[7]);
+        s.r = toFloat(vs[8]);
+        s.g = toFloat(vs[9]);
+        s.b = toFloat(vs[10]);
+        s.Ka = toFloat(vs[11]);
+        s.Kd = toFloat(vs[12]);
+        s.Ks = toFloat(vs[13]);
+        s.Kr = toFloat(vs[14]);
+        s.n = toFloat(vs[15]);
+        
+        spheres.push_back(s);
+    }
+    else if(vs[0] == "LIGHT"){
+        light l;
+        l.name = vs[1];
+        l.pos_x = toFloat(vs[2]);
+        l.pos_y = toFloat(vs[3]);
+        l.pos_z = toFloat(vs[4]);
+        l.Ir = toFloat(vs[5]);
+        l.Ig = toFloat(vs[6]);
+        l.Ib = toFloat(vs[7]);
+        
+        lights.push_back(l);
+    }
+    else if(vs[0] == "OUTPUT"){
+        outputFileName = vs[1];
     }
 }
 
@@ -186,6 +271,38 @@ void testForDebug ()
 {
     //This is the test for my debugging
     //Outputing the parsing result
+    ofstream outfile("testForDebug.txt");
+    outfile << "NEAR: " <<g_near << endl;
+    outfile << "LEFT: " <<g_left << endl;
+    outfile << "RIGHT: " << g_right << endl;
+    outfile << "BOTTOM: "<<g_bottom<<endl;
+    outfile << "TOP: " << g_top<<endl;
+    outfile << "RES: "<< g_width <<" "<< g_height<<endl;
+    vector<sphere>::iterator sph;
+    vector<light>::iterator li;
+    
+    for (sph = spheres.begin(); sph != spheres.end(); sph++) {
+        outfile << "SPHERES: ";
+        outfile << sph->name <<" "
+        <<sph->pos_x<<" "<<sph->pos_y<<" "<<sph->pos_z<<" "
+        <<sph->scl_x<<" "<<sph->scl_y<<" "<<sph->scl_z<<" "
+        <<sph->r<<" "<<sph->g<<" "<<sph->b<<" "
+        <<sph->Ka<<" "<<sph->Kd<<" "<<sph->Ks<<" "<<sph->Kr<<" "
+        <<sph->n<<endl;
+    
+    }
+    for (li = lights.begin(); li != lights.end(); li++) {
+        outfile << "LIGHTS: ";
+        outfile << li->name <<" "
+        <<li->pos_x<<" "<<li->pos_y<<" "<<li->pos_z<<" "
+        <<li->Ir << " " <<li->Ig<<" "<<li->Ib<<" "<<endl;
+        
+        
+    }
+    outfile <<"BACK: " <<back.r<<" "<<back.g<<" "<<back.b<<endl;
+    outfile <<"AMBIENT: "<<ambient.Ir<<" "<<ambient.Ig<<" "<<ambient.Ib<<endl;
+    outfile <<"OUTPUT: "<<outputFileName<<endl;
+    
 }
 
 
@@ -200,8 +317,15 @@ int main(int argc, char* argv[])
         exit(1);
     }
     loadFile(argv[1]);
-    render();
-    saveFile();
+    
+    
+    //
+    testForDebug();
+    //
+    
+    //REMEMBER TO UNCOMMENT THESE TWO LINES:
+    //render();
+    //saveFile();
 	return 0;
 }
 
